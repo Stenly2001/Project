059bfd321e365a8e84f2a9cab233294819d9dbdb
Stenly2001 <141318749+Stenly2001@users.noreply.github.com>
2024-01-08 09:10:55 +0000

    1

---
+++ model.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
